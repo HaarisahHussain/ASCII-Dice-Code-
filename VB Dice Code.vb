c8ac0a09daa700c77b19d23a0b71f3ebfd0debef
HaarisahHussain <108690684+HaarisahHussain@users.noreply.github.com>
2023-06-16 14:12:11 +0100

    Add files via upload

---
+++ VB Dice Code.vb
@@ -0,0 +1,19 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        'Visual Dice Code
+
+        Console.WriteLine("............")
+        Console.WriteLine(".     .")
+        Console.WriteLine(". #  # .")
+        Console.WriteLine(".  #  .")
+        Console.WriteLine(". #  # .")
+        Console.WriteLine(".     .")
+        Console.WriteLine("............")
+        Console.ReadLine()
+        'This code will output a series of dots, resembling a 6 sided dice
+
+
+    End Sub
+End Module
